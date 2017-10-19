87d52ac04bb4d75a6171e2940adeb2dc487aeb9d
bobbydowling6 <bobbydowling6@gmail.com>
2017-10-19 17:18:16 -0500

    created new content

---
+++ weather.js
@@ -0,0 +1,11 @@
+
+var callbackFunction = function(data) {
+    var wind = data.query.results.channel.wind;
+    alert(wind.chill);
+  };
+
+function weatherReceived(response){
+    console.log(response);
+
+$.getJSON('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', weatherReceived);
+}
