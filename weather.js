7c0ed10d453e33f5709b73a293472ec40bca0259
bobbydowling6 <bobbydowling6@gmail.com>
2017-10-20 15:00:52 -0500

    Made changes

--- weather.js
+++ weather.js
@@ -1,11 +1,22 @@
-
-var callbackFunction = function(data) {
-    var wind = data.query.results.channel.wind;
-    alert(wind.chill);
-  };
-
-function weatherReceived(response){
+function weatherReceived(response) {
     console.log(response);
 
-$.getJSON('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', weatherReceived);
+    var firstResult = response.query.results.channel.item.condition
+    console.log(firstResult);
+
+    var location = 'Unknown' // not returned in response
+    var temp = firstResult.temp
+    var text = firstResult.text
+
+    var x = ('The temperature is ' + temp + '. Forecast calls for ' + text + ' for today');
+
+    document.getElementById('output').innerHTML = x;
 }
+
+$("form").submit(function (event) {
+    event.preventDefault();
+
+    url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + $("#cityinput").val() + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+    $.getJSON(url, weatherReceived);
+
+});
